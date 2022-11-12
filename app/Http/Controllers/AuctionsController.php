110b055325a2ba1e78d9e773349cbc30802e2ad5
Catalyst546 <37144172+Catalyst546@users.noreply.github.com>
2022-11-12 22:51:53 +0000

    Created the auction controller methods

--- app/Http/Controllers/AuctionsController.php
+++ app/Http/Controllers/AuctionsController.php
@@ -37,49 +37,57 @@ class AuctionsController extends Controller
     public function manageAuctions()
     {
         $auctions = Auctions::all();
+        $categories = Category::all();
         $isAdmin = Controller::isAdmin();
-        return view('manage-auctions', compact('auctions', 'isAdmin'));
+        return view('manage-auctions', compact('categories','auctions', 'isAdmin'));
     }
 
     public function getAuction($id)
     {
         $auction = Auction::where('id', $id)->first();
+        $categories = Category::all();
         $isAdmin = Controller::isAdmin();
-        return view('edit-auction', compact('auction', 'isAdmin'));
+        return view('edit-auction', compact('categories', 'auction', 'isAdmin'));
     }
 
     public function deleteAuction($id)
     {
         Auction::destroy($id);
-        if(Category::find($id)>0)
+        if(Auction::find($id)>0)
             return redirect()->back()->with('error', 'Auction could not be deleted');
         return redirect()->back()->with('message', 'Auction has been deleted successfully');
     }
 
 
     public function addAuction(Request $request){
-        $title = $request->new_auction_name;
+        $title = $request->new_auction_title;
         $description = $request->new_auction_description;
+        $category = $request->new_auction_category;
         if(isset($title) && trim($title) != ""){
             $title_found = Auction::where('name',$title)->first();
             if (empty($title_found->id)){
                 $save = new Auction();
                 $save->id=Str::uuid();
-                $save->name=$title;
+                $save->title=$title;
                 if(!isset($description) || trim($description) != ""){
-                    $description = "All ".$title;
+                    $description = "Auction on ".$title;
                 }
                 $save->description=$description;
+                $category_found = Category::where('id',$category)->first();
+                if (empty($category_found)) {
+                    return redirect()->route('auction.manage')->with('Error', 'Category does not exist');
+                }
+                $save->category_id=$category;
                 $save->created_by=Controller::getUsername();
                 $save->save();
 
                 return redirect()->route('auction.manage')->with('message', 'Auction has been successfully added');
             }else{
-                return redirect()->route('auction.manage')->with('Error', 'Auction name is already available');
+                return redirect()->route('auction.manage')->with('Error', 'Auction title is already available');
             }
 
         }
-        return redirect()->route('auction.manage')->with('Error', 'Auction name cannot be empty');
+        return redirect()->route('auction.manage')->with('Error', 'Auction title cannot be empty');
     }
 
     public function editAuctionForm($id){
@@ -87,13 +95,15 @@ class AuctionsController extends Controller
         if (empty($auction)){
             return redirect()->back()->with('Error', 'Auction does not exist');
         }
+        $categories = Category::all();
         $isAdmin = $this->isAdmin();
-        return view('auctions-form', compact('auction', 'isAdmin'));
+        return view('auctions-form', compact('categories','auction', 'isAdmin'));
     }
 
     public function editAuction(Request $request, $id){
         $title = $request->new_auction_name;
         $description = $request->new_auction_description;
+        $selectedCategory = $request->new_auction_category;
         if(isset($title) && trim($title) != "" && trim($description) != "" ){
             $auction = Category::where('id', $id)->first();
             if (!empty($auction)){
@@ -102,14 +112,19 @@ class AuctionsController extends Controller
                     $description = "All ".$title;
                 }
                 $auction->description=$description;
+                $category_found = Category::where('id',$selectedCategory)->first();
+                if (empty($category_found)) {
+                    return redirect()->route('auction.manage')->with('Error', 'Category does not exist');
+                }
+                $auction->category_id=$selectedCategory;
                 $auction->save();
 
                 return redirect()->route('auction.manage')->with('message', 'Auction has been successfully added');
             }else{
-                return redirect()->back()->with('Error', 'Auction name is already available');
+                return redirect()->back()->with('Error', 'Auction title is already available');
             }
 
         }
-        return redirect()->back()->with('Error', 'Auction name cannot be empty');
+        return redirect()->back()->with('Error', 'Auction title cannot be empty');
     }
 }
